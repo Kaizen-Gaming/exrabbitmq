6455623db0a7fcac0eaf7696c2cc2e2d7a23947f
Spyridon Alfredos Desyllas <sadesyllas@gmail.com>
2017-08-31 11:41:35 +0000

    Release v2.4.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule ExRabbitMQ.Mixfile do
 
   def project do
     [app: :exrabbitmq,
-     version: "2.3.0",
+     version: "2.4.0",
      elixir: "~> 1.5",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
